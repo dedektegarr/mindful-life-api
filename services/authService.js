74a3cfa1d1ee8bf43d032d65241d99682e66189a
Dedek Tegar Apriyandi <dedektegarr@gmail.com>
2024-06-05 13:19:16 +0700

    refact: refactor authService code

--- services/authService.js
+++ services/authService.js
@@ -1,8 +1,8 @@
 const { addDoc, collection, serverTimestamp } = require("@firebase/firestore");
 const { db } = require("../config/firebase");
 
-const createUser = async ({ name, username, email, password }) => {
-  const userData = { name, username, email, role: "user", password };
+const createUser = async (data) => {
+  const userData = { ...data, role: "user" };
 
   const user = await addDoc(collection(db, "users"), {
     ...userData,
